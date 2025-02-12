d78094e8d2327db83311f1a1f5b0e615e2a66b32
darlingson <dmakuwila@gmail.com>
2025-02-12 21:44:16 +0200

    learnt data types

---
+++ data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/home/darlingson/rust-learning-path/data_types/target/debug/data_types: /home/darlingson/rust-learning-path/data_types/src/main.rs
---
+++ data_types/target/debug/deps/data_types-043f1139bdd5e355.d
@@ -0,0 +1,5 @@
+/home/darlingson/rust-learning-path/data_types/target/debug/deps/data_types-043f1139bdd5e355: src/main.rs
+
+/home/darlingson/rust-learning-path/data_types/target/debug/deps/data_types-043f1139bdd5e355.d: src/main.rs
+
+src/main.rs:
